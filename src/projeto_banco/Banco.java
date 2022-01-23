0eff5bf2302f1ef3f9d298a2ef5bf291a94159eb
LucasEPO <lucas.epoliveira@gmail.com>
2022-01-23 18:37:17 -0300

    Adicionando classe Banco

---
+++ src/projeto_banco/Banco.java
@@ -0,0 +1,19 @@
+package projeto_banco;
+
+/**
+ *
+ * @author LucasEPO
+ */
+public class Banco {
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
