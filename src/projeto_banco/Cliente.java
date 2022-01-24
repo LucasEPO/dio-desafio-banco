f10938679fd3ee7eecdb22dd640c5c5bfd4dc0b3
LucasEPO <lucas.epoliveira@gmail.com>
2022-01-23 23:07:45 -0300

    Atualizando atributos da classe cliente

--- src/projeto_banco/Cliente.java
+++ src/projeto_banco/Cliente.java
@@ -7,6 +7,14 @@ package projeto_banco;
 public class Cliente {
 
     private String nome;
+    private int idade;
+    private double salario;
+
+    public Cliente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = 0;
+    }
 
     public String getNome() {
         return nome;
@@ -14,6 +22,22 @@ public class Cliente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
     }
     
 }
